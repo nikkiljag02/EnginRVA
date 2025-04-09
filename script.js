0711301c59adda0445890f968787465bbbd6eec7
nikkiljag02 <37817745+nikkiljag02@users.noreply.github.com>
2025-04-08 20:50:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+  
+    const options = {
+      threshold: 0.2
+    };
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add("animate");
+        }
+      });
+    }, options);
+  
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
+  
